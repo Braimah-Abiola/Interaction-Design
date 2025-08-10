9eff050d990998abaf0098c28bdd9b99aaae45d8
Braimah-Abiola <braimahabiola.official@gmail.com>
2025-08-10 22:45:17 +0000

    Fix routes

--- animated-buttons/src/app/page.tsx
+++ animated-buttons/src/app/page.tsx
@@ -1,17 +1,11 @@
-import ThreadsButton from "./components/buttons/button-one";
-import GlowButton from "./components/buttons/button-three";
 import FlowButton from "./components/buttons/button-two";
 
 export default function Home() {
   return (
     <main>
-      {/* Flow Button */}
       <div className="w-screen h-screen bg-black items-center justify-center flex">
         <FlowButton text="Visit Studio IX Today" />
       </div>
-      {/* Flow Button */}
-
-     
     </main>
   );
 }
