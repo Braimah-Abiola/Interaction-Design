9b18da37beb56df5e70cb14c5038b2576430a809
Braimah-Abiola <braimahabiola.official@gmail.com>
2025-08-20 22:44:48 +0000

    Update bg color

--- animated-buttons/src/app/(routes)/tilt-headline/page.tsx
+++ animated-buttons/src/app/(routes)/tilt-headline/page.tsx
@@ -3,7 +3,7 @@ import { Merge } from 'lucide-react';
 
 const TiltHeadlinePage = () => {
     return (
-        <div className=" w-full h-screen flex items-center justify-center bg-[#FFEF9A]/50">
+        <div className=" w-full h-screen flex items-center justify-center bg-[#FFF7CC]">
             <TiltHeadline badgeIcon={Merge} badgeText="Beta" href="/" mainText="Get Early Access Now" />
         </div>
     );
