65aa2da5f405b1569b1156a23583e17f9b905b1d
Braimah-Abiola <braimahabiola.official@gmail.com>
2025-08-26 19:57:57 +0000

    Update faq section

--- faq-sections/src/app/page.tsx
+++ faq-sections/src/app/page.tsx
@@ -2,7 +2,7 @@ import Faqs from "@/sections/faqs";
 
 const Home = () => {
   return (
-    <main className=" h-screen w-full flex items-center justify-center">
+    <main className=" min-h-screen w-full flex items-center justify-center">
       <Faqs />
     </main>
   );
